05c123ed8b5ee214fb34f405ace87004ccaf40cc
hexu <hexu_1985@sina.com>
2021-06-03 17:39:20 +0800

    generate_linux_proj.sh增加Debug编译选项

--- Chapter01/generate_linux_proj.sh
+++ Chapter01/generate_linux_proj.sh
@@ -3,5 +3,5 @@
 current_dir=$(cd "$(dirname "$0")";pwd)
 echo $current_dir
 
-cmake -H"$current_dir/" -B"$current_dir/platforms_project/linux" 
+cmake -H"$current_dir/" -B"$current_dir/platforms_project/linux" -DCMAKE_BUILD_TYPE=Debug
 
